c22db70ac390ec853643a6e61e6de627cfd24aa2
Alexey Komissarouk <alexey@alexeymk.com>
2013-11-14 18:49:02 -0800

    updated description

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
-	summary: "Node-Stripe brought to Meteor."
+  summary: "Node-Stripe brought to Meteor.  Server-only fork of https://github.com/appleifreak/stripe-meteor"
 });
 
 Npm.depends({ "stripe": "2.1.0-rc3" });
 
 Package.on_use(function (api) {
-	
+
 	 // ensure backwards compatibility with Meteor pre-0.6.5
 	if (api.export){
 	    api.export('STRIPEMETEOR');
